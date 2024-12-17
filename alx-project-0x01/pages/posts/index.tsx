74e419f989b38f6c89b55e9f856509069ed84765
WorkuUrga <urgaworku@gmail.com>
2024-12-17 15:43:44 +0300

    Setting up Project-1

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,12 +1,74 @@
-import React from "react";
 import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
+import Header from "@/components/layout/Header";
+import { PostData, PostProps } from "@/interfaces";
+import { useState } from "react";
 
-const PostsPage: React.FC = () => {
-    return (
-        <div>
-            <h1>Posts</h1>
-            <PostCard title='First Card' description="This is the first card" />
-        </div>
-    )
+interface PostsPageProps {
+  posts: PostProps[];
 }
-export default PostsPage
+
+const Posts: React.FC<PostsPageProps> = ({ posts }) => {
+  console.log(posts);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [post, setPost] = useState<PostData | null>(null);
+
+  const handleAddPost = (newPost: PostData) => {
+    setPost({ ...newPost, id: posts.length + 1 });
+  };
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">Post Content</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
+            Add Post
+          </button>
+        </div>
+        <div className="grid grid-cols-3 gap-2">
+          {posts?.map(({ title, body, userId, id }: PostProps) => (
+            <PostCard
+              title={title}
+              body={body}
+              userId={userId}
+              id={id}
+              key={id}
+            />
+          ))}
+        </div>
+      </main>
+
+      {isModalOpen && (
+        <PostModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />
+      )}
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts: PostProps[] = await response.json();
+
+    return {
+      props: {
+        posts,
+      },
+    };
+  } catch (error) {
+    console.error("Failed to fetch posts:", error);
+
+    return {
+      props: {
+        posts: [],
+      },
+    };
+  }
+}
+
+export default Posts;
